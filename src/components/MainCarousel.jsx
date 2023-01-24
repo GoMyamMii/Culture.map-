1e55198b6416214d25abbf23e23d1e84476f0c3d
seoyeon-jung <jungluchia1204@gmail.com>
2023-01-24 12:29:35 +0900

    fix: carousel api 이미지로 수정(#3) - carousel 이미지를 api에서 받아온 이미지로 변경했습니다 @seoyeon-jung

--- src/components/MainCarousel.jsx
+++ src/components/MainCarousel.jsx
@@ -1,25 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import styled from 'styled-components';
+import CarouselImg from './CarouselImg';
+import { getSearchData } from '../api';
+import { useQuery } from 'react-query';
 
-const MainSlide = () => {
+const MainCarousel = () => {
+  const [cityValue, setCityValue] = useState('');
+  const [titleValue, setTitleValue] = useState('');
+
+  const { data: selectData, isLoading: selectLoading } = useQuery(
+    ['cityData', cityValue, titleValue],
+    getSearchData
+  );
+
+  if (selectLoading) {
+    <div>로딩중입니다.</div>;
+  }
+
   return (
     <Wrapper>
       <StyledSlider {...settings}>
-        <CarouselBox>
-          <CarouselImg src="http://www.cha.go.kr/unisearch/images/national_treasure/2685609.jpg" />
-        </CarouselBox>
-        <CarouselBox>
-          <CarouselImg src="http://www.cha.go.kr/unisearch/images/national_treasure/1611449.jpg" />
-        </CarouselBox>
-        <CarouselBox>
-          <CarouselImg src="http://www.cha.go.kr/unisearch/images/national_treasure/1611458.jpg" />
-        </CarouselBox>
-        <CarouselBox>
-          <CarouselImg src="http://www.cha.go.kr/unisearch/images/national_treasure/1612029.jpg" />
-        </CarouselBox>
+        {selectData?.flat().map((item) => (
+          <CarouselImg item={item} key={item.id} />
+        ))}
       </StyledSlider>
     </Wrapper>
   );
@@ -43,18 +49,8 @@ const Wrapper = styled.div`
 `;
 
 const StyledSlider = styled(Slider)`
-  background-color: gray;
-`;
-
-const CarouselBox = styled.div`
-  /* width: 100%;
-  background-color: lightgrey; */
-`;
-
-const CarouselImg = styled.img`
   width: 100%;
-  height: auto;
-  object-fit: cover;
+  height: 800px;
 `;
 
-export default MainSlide;
+export default MainCarousel;

6a0c67ba3fed30e5dd4b409b66c5ff2a64c4a755
Jaehyun <vgb30xk@naver.com>
2023-01-25 13:14:27 +0900

    각 컴포넌트가 점점 나타나게 하는부분은 구현했지만, 스크롤을 내릴때 하나씩 나타나도록 나오는 부분은 아직 고민중(주석처리했음)

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,13 +1,21 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { useLocation } from 'react-router-dom';
 import styled from 'styled-components';
 import noImg from '../image/no-image.png';
+import { Fade } from 'react-reveal';
+import '../scroll.css';
+
 const { kakao } = window;
 const Detail = () => {
   const location = useLocation();
   const detailData = location.state;
   const { title, name, long, leti, content, gene, date, position, image } =
     detailData;
+
+  // const containerRef = useRef();
+  // const AcontainerRef = useRef();
+  // const BcontainerRef = useRef();
+  // const CcontainerRef = useRef();
 
   useEffect(() => {
     if (!document.getElementById('map')) {
@@ -23,41 +31,74 @@ const Detail = () => {
     }
   }, []);
 
+  // useEffect(() => {
+  //   const observer = new IntersectionObserver((entries) => {
+  //     entries.forEach((entry) => {
+  //       if (entry.isIntersecting) {
+  //         entry.target.style.opacity = 1;
+  //         entry.target.style.transition = 'opacity 1s';
+  //         observer.unobserve(entry.target);
+  //       }
+  //     });
+  //   });
+
+  //   observer.observe(containerRef.current);
+  //   observer.observe(AcontainerRef.current);
+  //   observer.observe(BcontainerRef.current);
+  //   observer.observe(CcontainerRef.current);
+  //   return () => {
+  //     observer.disconnect();
+  //   };
+  // }, []);
+
   return (
     <Container>
       <Acontainer>
-        <A1container>
-          {image ? (
-            <img
-              src={image}
-              style={{
-                height: 400,
-                width: 400,
-                borderRadius: '20px',
-              }}
-              alt="img"
-            />
-          ) : (
-            <img src={noImg} style={{ height: 400, width: 400 }} alt="img" />
-          )}
-        </A1container>
-        <A2container>
-          <Name> {name === ('' || null) ? '없음' : name}</Name>
+        <Fade duration={2000} delay={500}>
+          <A1container>
+            {image ? (
+              <img
+                src={image}
+                style={{
+                  height: 400,
+                  width: 400,
+                  borderRadius: '20px',
+                }}
+                alt="img"
+              />
+            ) : (
+              <img src={noImg} style={{ height: 400, width: 400 }} alt="img" />
+            )}
+          </A1container>
+        </Fade>
 
-          <SubName>종목 : {title === ('' || null) ? '없음' : title}</SubName>
-          <SubName>등록일 : {date === undefined ? '미상' : date}</SubName>
-          <SubName>시대 : {gene === '' ? '미상' : gene}</SubName>
-          <SubName>소재지 : {position === '' ? '없음' : position}</SubName>
+        <A2container>
+          <Fade duration={4000} delay={1000}>
+            <Name>{name === ('' || null) ? '없음' : name}</Name>
+          </Fade>
+          <Fade duration={6000} delay={2000}>
+            <SubName>종목 : {title === ('' || null) ? '없음' : title}</SubName>
+            <SubName>등록일 : {date === undefined ? '미상' : date}</SubName>
+            <SubName>시대 : {gene === '' ? '미상' : gene}</SubName>
+            <SubName>소재지 : {position === '' ? '없음' : position}</SubName>
+          </Fade>
         </A2container>
       </Acontainer>
 
-      <Bcontainer>내용 : {content === '' ? '없음' : content}</Bcontainer>
-      <Ccontainer>
-        {' '}
-        {long !== 0 ? (
-          <div id="map" style={{ height: 400, width: 800 }}></div>
-        ) : null}
-      </Ccontainer>
+      <Fade duration={8000} delay={4000}>
+        <Bcontainer>내용 : {content === '' ? '없음' : content}</Bcontainer>
+      </Fade>
+
+      <Fade duration={10000} delay={8000}>
+        <Ccontainer>
+          {long !== 0 ? (
+            <div
+              id="map"
+              style={{ height: 400, width: '100%', borderRadius: '20px' }}
+            ></div>
+          ) : null}
+        </Ccontainer>
+      </Fade>
     </Container>
   );
 };
@@ -82,7 +123,7 @@ const A2container = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  margin-left: 100px;
+  margin-left: 50px;
 `;
 
 const Name = styled.div`

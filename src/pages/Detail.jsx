b3dce5e9cde8797aeba101e74bf81e340a9e367e
Jaehyun <vgb30xk@naver.com>
2023-01-25 11:30:41 +0900

    Detail css 뼈대 생성

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
+import styled from 'styled-components';
 import noImg from '../image/no-image.png';
 const { kakao } = window;
 const Detail = () => {
@@ -7,6 +8,7 @@ const Detail = () => {
   const detailData = location.state;
   const { title, name, long, leti, content, gene, date, position, image } =
     detailData;
+
   useEffect(() => {
     if (!document.getElementById('map')) {
       console.log('무야호');
@@ -22,23 +24,82 @@ const Detail = () => {
   }, []);
 
   return (
-    <div>
-      {image ? (
-        <img src={image} style={{ height: 400, width: 400 }} alt="img" />
-      ) : (
-        <img src={noImg} style={{ height: 400, width: 400 }} alt="img" />
-      )}
-      <div>종목 : {title === ('' || null) ? '없음' : title}</div>
-      <div>이름 : {name === ('' || null) ? '없음' : name}</div>
-      <div>시대 : {gene === '' ? '미상' : gene}</div>
-      <div>등록일 : {date === undefined ? '미상' : date}</div>
-      <div>소재지 : {position === '' ? '없음' : position}</div>
-      <div>내용 : {content === '' ? '없음' : content}</div>
-      {long !== 0 ? (
-        <div id="map" style={{ height: 400, width: 400 }}></div>
-      ) : null}
-    </div>
+    <Container>
+      <Acontainer>
+        <A1container>
+          {image ? (
+            <img
+              src={image}
+              style={{
+                height: 400,
+                width: 400,
+                borderRadius: '20px',
+              }}
+              alt="img"
+            />
+          ) : (
+            <img src={noImg} style={{ height: 400, width: 400 }} alt="img" />
+          )}
+        </A1container>
+        <A2container>
+          <Name> {name === ('' || null) ? '없음' : name}</Name>
+
+          <SubName>종목 : {title === ('' || null) ? '없음' : title}</SubName>
+          <SubName>등록일 : {date === undefined ? '미상' : date}</SubName>
+          <SubName>시대 : {gene === '' ? '미상' : gene}</SubName>
+          <SubName>소재지 : {position === '' ? '없음' : position}</SubName>
+        </A2container>
+      </Acontainer>
+
+      <Bcontainer>내용 : {content === '' ? '없음' : content}</Bcontainer>
+      <Ccontainer>
+        {' '}
+        {long !== 0 ? (
+          <div id="map" style={{ height: 400, width: 800 }}></div>
+        ) : null}
+      </Ccontainer>
+    </Container>
   );
 };
 
 export default Detail;
+
+const Container = styled.div`
+  margin-inline: 100px;
+  margin-top: 50px;
+  margin-bottom: 50px;
+`;
+
+const Acontainer = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
+const A1container = styled.div``;
+
+const A2container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-left: 100px;
+`;
+
+const Name = styled.div`
+  text-align: center;
+  font-size: 50px;
+  font-weight: 700;
+  margin-bottom: 50px;
+`;
+const SubName = styled.div`
+  font-size: 20px;
+  font-weight: 500;
+  margin-bottom: 5px;
+`;
+
+const Bcontainer = styled.div`
+  margin-top: 50px;
+`;
+const Ccontainer = styled.div`
+  margin-top: 50px;
+`;

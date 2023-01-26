8642aec33baa68122a98bc70672d5fc4396f6ac6
kimtanook <seob6402@gmail.com>
2023-01-26 23:25:36 +0900

    fix: review delete modal (#14) 리뷰를 삭제 완료 후 하얀 배경만 보이는 부분을 수정했습니다. by @kimtanook

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -23,6 +23,7 @@ export default function Modal({
       deleteMutate(item, {
         onSuccess: () => {
           queryClient.invalidateQueries('reviews');
+          setDeleteToggle(false);
         },
       });
     } else {

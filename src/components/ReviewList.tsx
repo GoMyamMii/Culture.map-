e48dea27fadcb0dcb3a922258166b9a3449f48f0
kimtanook <seob6402@gmail.com>
2023-01-26 17:28:25 +0900

    feat: create review c.r.d. (#14) 리뷰 기능에 필요한 컴포넌트를 만들었습니다. by @kimtanook

--- src/components/ReviewList.tsx
+++ src/components/ReviewList.tsx
@@ -1,14 +1,21 @@
-import React, { ChangeEvent, useState } from 'react';
+import React, { ChangeEvent, FormEvent, useState } from 'react';
 import { useMutation, useQueryClient } from 'react-query';
 import styled from 'styled-components';
 import { createReview } from '../api';
 import ReviewItem from './ReviewItem';
 
-function ReviewList() {
+const ReviewList = ({
+  itemData,
+  cultureId,
+}: {
+  itemData: reviewType[];
+  cultureId: string;
+}) => {
   const queryClient = useQueryClient();
   const [name, setName] = useState('');
   const [password, setPassword] = useState('');
   const [body, setBody] = useState('');
+
   const { isLoading: createLoading, mutate: createMutate } =
     useMutation(createReview);
   const onChangeName = (event: ChangeEvent<HTMLInputElement>) => {
@@ -20,37 +27,63 @@ function ReviewList() {
   const onBodyName = (event: ChangeEvent<HTMLInputElement>) => {
     setBody(event.target.value);
   };
-  const submitReview = () => {
+  const submitReview = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
     createMutate(
-      { name, password, body },
+      { cultureId, name, password, body },
       {
         onSuccess: () => {
           queryClient.invalidateQueries('reviews');
         },
       }
     );
-    if (!createLoading) {
-      setName('');
-      setPassword('');
-      setBody('');
-    }
+    setName('');
+    setPassword('');
+    setBody('');
   };
+
+  if (createLoading) {
+    return <div> 리뷰를 작성중입니다.</div>;
+  }
+
   return (
     <div>
-      <div>
-        <NameInput onChange={onChangeName} placeholder="닉네임" />
-        <PasswordInput onChange={onPasswordName} placeholder="비밀번호" />
-      </div>
-      <div>
-        <BodyInput onChange={onBodyName} placeholder="내용" />
-      </div>
-      <div>
-        <button onClick={submitReview}>작성</button>
-      </div>
-      <ReviewItem />
+      <form onSubmit={submitReview}>
+        <div>
+          <NameInput
+            maxLength={10}
+            onChange={onChangeName}
+            value={name}
+            placeholder="닉네임"
+            required
+          />
+          <PasswordInput
+            maxLength={4}
+            onChange={onPasswordName}
+            value={password}
+            placeholder="비밀번호"
+            required
+          />
+        </div>
+        <div>
+          <BodyInput
+            maxLength={48}
+            onChange={onBodyName}
+            value={body}
+            placeholder="내용"
+            required
+          />
+        </div>
+        <div>
+          <button>작성</button>
+        </div>
+      </form>
+      {itemData?.map((item: reviewType) => (
+        <ReviewItem key={item.id} item={item} />
+      ))}
     </div>
   );
-}
+};
 
 export default ReviewList;
 
